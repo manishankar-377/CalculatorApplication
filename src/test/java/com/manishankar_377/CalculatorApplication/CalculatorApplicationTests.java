c546eae82f646829888f00c201463ab5d9e53156
Mani Shankar <manishankar377@gmail.com>
2025-05-13 10:16:18 -0500

    intial commit

---
+++ src/test/java/com/manishankar_377/CalculatorApplication/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manishankar_377.CalculatorApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
